c021ba0d727be34f2be6b9aa206f76941c1e8ac3
Lukilah <111893154+Lukilah@users.noreply.github.com>
2024-05-28 09:57:52 +0300

    Create list-items.js
    Renders a list of my favorite games using React & React DOM

---
+++ list-items.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>This is Lukilah's favorite games</h1>
+    <ul>
+      <li>Wolfenstein</li>
+      <li>GTA 5</li>
+      <li>Far Cry 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
