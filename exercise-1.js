d31f3fd13c4105a5de2a02376ec8fae9a3633a55
Lukilah <111893154+Lukilah@users.noreply.github.com>
2024-05-28 09:42:33 +0300

    Create exercise-1.js
    Highlights how JSX works especially how we can render HTML in a JS file.

---
+++ exercise-1.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//render only takes 1 HTML element
+ReactDOM.render(
+  <div>
+    <h1>Hello World! This is my first react project.</h1>
+    <p>I think React is pretty fun to learn.</p>
+  </div>,
+  document.getElementById("root")
+);
